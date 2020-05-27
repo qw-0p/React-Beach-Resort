70a21c65f5aeb41e07622eb52f493d4739052d7d
Aleksandr Sarkisov <quopiks@gmail.com>
2020-05-27 22:36:01 +0300

    added Banner component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import Hero from '../componennts/Hero';
+import Banner from '../componennts/Banner';
+import {Link} from 'react-router-dom';
 
 export default function Home() {
-    return <Hero hero="defaultHero"></Hero>;
+    return (
+        <Hero hero="defaultHero">
+            <Banner
+                title="luxurious rooms"
+                subtitle="deluxe rooms starting at $299"
+            >
+                <Link to="/rooms" className="btn-primary">
+                    our rooms
+                </Link>
+            </Banner>
+        </Hero>
+    );
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import Hero from '../componennts/Hero';
+import Banner from '../componennts/Banner';
+import {Link} from 'react-router-dom';
 
 export default function Rooms() {
-    return <Hero hero="roomsHero"></Hero>;
+    return (
+        <Hero hero="roomsHero">
+            <Banner title="our rooms">
+                <Link to="/" className="btn-primary">
+                    return home
+                </Link>
+            </Banner>
+        </Hero>
+    );
 }
