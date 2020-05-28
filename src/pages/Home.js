ef4e048a555cf269e253bb0ebaf58b5bacd70e06
Aleksandr Sarkisov <quopiks@gmail.com>
2020-05-28 13:43:34 +0300

    created Services and Title component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,22 @@ import React from 'react';
 import Hero from '../componennts/Hero';
 import Banner from '../componennts/Banner';
 import {Link} from 'react-router-dom';
+import Services from '../componennts/Services';
 
 export default function Home() {
     return (
-        <Hero hero="defaultHero">
-            <Banner
-                title="luxurious rooms"
-                subtitle="deluxe rooms starting at $299"
-            >
-                <Link to="/rooms" className="btn-primary">
-                    our rooms
-                </Link>
-            </Banner>
-        </Hero>
+        <>
+            <Hero hero="defaultHero">
+                <Banner
+                    title="luxurious rooms"
+                    subtitle="deluxe rooms starting at $299"
+                >
+                    <Link to="/rooms" className="btn-primary">
+                        our rooms
+                    </Link>
+                </Banner>
+            </Hero>
+            <Services />
+        </>
     );
 }
