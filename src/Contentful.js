79c8fb31ddf3236dfc00d127e1a0a9d1e1774469
Aleksandr Sarkisov <quopiks@gmail.com>
2020-06-01 21:39:06 +0300

    added process.env

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-    space: '1qvf1hfakv71',
-    accessToken: 'RNF56DYm5iUqXa0trc_Hg2NxREX81Ykw1oeCDzYCBkU',
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
